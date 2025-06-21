1ee3e3c62b394bcc317b6a7cca3073e1479bb9f1
lethalstudio <>
2025-06-21 11:35:05 +0600

    Update

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 print("Hello! Please Register!")
 while True:
     nickname = input("Name~> ")
@@ -11,18 +10,4 @@ while True:
         break
     else:
         print("Incorrect Name or Password")
-=======
-print("Hello! Please Register!")
-while True:
-    nickname = input("Name~> ")
-    password = input("Password~> ")
-    with open('C:/pj/Python/db.txt', 'r') as file:
-        content = file.read()
-    if f"{nickname} {password}" in content:
-        print("Successful Login")
-        input("Press ENTER to exit...")
-        break
-    else:
-        print("Incorrect Name or Password")
->>>>>>> 136c7ff90a84c0d69493b0654692f38e85e31b4e
         continue
